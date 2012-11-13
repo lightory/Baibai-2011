46e70272ccfa11f319a099970d3a64f770839ecf
LIGHT <lightory@gmail.com>
2012-11-13 23:24:18 +0800

    remove QQWeibo&Twitter link in sidebar

--- application/views/account/apply.php
+++ application/views/account/apply.php
@@ -63,14 +63,8 @@
 				  <a href="http://t.sina.com.cn/baibaibook/" class="sidebar_snsLinks_link" target="_blank">
 				    <img src="<?php echo site_url('include/style/img').'tsina.png'; ?>" />
 				  </a>
-				  <a href="http://t.qq.com/baibaibook/" class="sidebar_snsLinks_link" target="_blank">
-            <img src="<?php echo site_url('include/style/img').'tqq.png'; ?>" />
-          </a>
           <a href="http://www.douban.com/people/baibaibook/" class="sidebar_snsLinks_link" target="_blank">
             <img src="<?php echo site_url('include/style/img').'dou.png'; ?>" />
-          </a>
-          <a href="http://twitter.com/baibaibook/" class="sidebar_snsLinks_link" target="_blank">
-            <img src="<?php echo site_url('include/style/img').'twitter.png'; ?>" />
           </a>
 				</div>
       </div>
